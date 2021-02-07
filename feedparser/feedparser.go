7b009d332a13c084a343eb89a3a6bd4adde41f08
Lucas Charles <me@lucascharles.me>
2021-02-07 12:25:28 -0800

    fix: Add 30sec timeout to url parsing

--- feedparser/feedparser.go
+++ feedparser/feedparser.go
@@ -1,19 +1,25 @@
 package feedparser
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/mmcdole/gofeed"
 )
+
+const timeoutSeconds = 30
 
 // Latest returns the list of latest items
 func Latest(maxItems int) []*gofeed.Item {
 	feedUrl := os.Getenv("RSS_FEED_URL")
 
 	items := []*gofeed.Item{}
+	ctx, cancel := context.WithTimeout(context.Background(), timeoutSeconds*time.Second)
+	defer cancel()
 	fp := gofeed.NewParser()
-	feed, error := fp.ParseURL(feedUrl)
+	feed, error := fp.ParseURLWithContext(feedUrl, ctx)
 	if error != nil {
 		fmt.Printf("Error parsing %v\n%v\n", feedUrl, error.Error())
 		os.Exit(1)
